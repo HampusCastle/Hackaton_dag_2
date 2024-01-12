4b358b52a4e4a28c27ed89ad0b9e06c4e2df70ce
HampusCastle <Hampuscastle@gmail.com>
2024-01-12 13:22:18 +0100

    Hackaton project day 2

--- src/main/java/org/example/HashFunction.java
+++ src/main/java/org/example/HashFunction.java
@@ -1,9 +1,22 @@
 package org.example;
 
+
 public class HashFunction {
 
-    public String hash(String input) {
-        // TODO: Implement this
-        return null;
+    public static String hash(String input) {
+        return generateRandomHash(input);
+    }
+
+    private static String generateRandomHash(String input) {
+        int originalHash = Math.abs(input.hashCode()); // Computes the hash code of the input and ensures it's non-negative.
+        String lastHash = Integer.toString(originalHash, 32); // Converts the hash code to a base-32 string.
+
+        while (lastHash.length() < 32) {
+            lastHash += lastHash; // Appends the current hash to itself until the string is at least 32 characters long.
+        }
+
+        return lastHash.substring(0, 32); // Returns the first 32 characters of the resulting string.
     }
 }
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
         HashFunction hashFunction = new HashFunction();
 
-        System.out.println(hashFunction.hash("Fredrik123"));
+        String pass = getUserInput("Enter password: ");
+
+        String hash1 = hashFunction.hash(pass);
+
+        System.out.println(pass);
+        System.out.println(hash1);
+    }
+
+    public static String getUserInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(prompt);
+        String input = scanner.nextLine();
+
+        return input;
     }
 }
+
